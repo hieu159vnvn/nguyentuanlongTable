544ebe0bf36110cc3e2e0bc4c0b7d3b20d58dd9b
hieu159vnvn <hieu159vnvn@gmail.com>
2025-12-05 21:53:06 +0700

    fix QR

--- web/src/app/invoice/page.tsx
+++ web/src/app/invoice/page.tsx
@@ -828,7 +828,7 @@ export default function InvoicePage() {
                       <div className="text-center">
                         {bankInfo.qrImage?.url ? (
                           <Image 
-                            src={`${process.env.NEXT_PUBLIC_API_URL || 'images/QR-code.png' || 'http://localhost:1337'}${bankInfo.qrImage.url }`}
+                            src={`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'}${ '/images/QR-code.png' }`}
                             alt="QR Code"
                             style={{ 
                               maxWidth: '200px', 
